a04b82fbff4fe72e53e58ac5ca38913ec074d115
Lavi Singh <lavi89271@gmail.com>
2026-08-15 21:30:35 +0530

    Time: 2 ms (2.84%), Space: 12.8 MB (13.37%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int>ans(nums1.begin(),nums1.begin()+m);
+       ans.insert(ans.end(),nums2.begin(),nums2.end());
+       sort(ans.begin(),ans.end());
+       nums1=ans;
+    }
+};
